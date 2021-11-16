fbe0aa1610ccfb53594ac85e528ddf9f6031bb9e
valteros1 <79317752+valteros1@users.noreply.github.com>
2021-11-16 21:04:42 +0200

    first basid db

---
+++ database/database.ts
@@ -0,0 +1,38 @@
+
+const db = {
+    users: [
+    {
+        id: 1,
+        firstName: 'Valter',
+        lastName: 'Rosenfeld',
+        
+    },
+    {
+        id: 2,
+        firstName: 'Toomas',
+        lastName: 'Joomas',
+    }
+    ],
+    challengesBody: [
+    {
+        id: 1,
+        Challenge: 'Tee midagi kehale'
+    }
+    ],
+    challengeMind: [
+    {   
+        id: 1,
+        Challenge: 'Tee midagi vaimule'
+
+    }     
+    ],
+    challengeStomach: [
+    {
+        id: 1,
+        Challenge: 'Midagi kõhule'
+
+    }
+    ],
+}
+
+export default db;
