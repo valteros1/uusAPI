bd4b2e12411c40ae78bafe42ce2f94648f2b9954
valteros1 <79317752+valteros1@users.noreply.github.com>
2022-01-04 17:29:55 +0200

    tests2

--- tests/challengesBody.test.ts
+++ tests/challengesBody.test.ts
@@ -46,12 +46,60 @@ describe('challengesBody controller', () => {
             expect(response.body).to.be.a('object');
             expect(response.statusCode).to.equal(200);
             expect(response.body).to.have.key('data');
-            expect(response.body.challengesBody).to.be.a('array');
-            expect(response.body.challengesBody.length).to.greaterThan(0);
+            expect(response.body.data).to.be.a('array');
+            expect(response.body.data.length).to.greaterThan(0);
           });
          
     });
 }); 
+
+describe('challengesBody controller', () => {
+    describe('GET /challengesBody/1', () => {
+      it('responds with code 200 and token after login', async () => {
+        const response = await request(app)
+          .post('/login')
+          .send(user);
+        expect(response.body).to.be.a('object');
+        expect(response.statusCode).to.equal(200);
+        expect(response.body).to.have.key('token');
+        expect(response.body.token).to.be.a('string');
+        token = response.body.token;
+      });
+
+        it('responds with code 404 and error message', async () => {
+            const response = await request(app).get('/challengesBody/1');
+            expect(response.body).to.be.a('object');
+            expect(response.statusCode).to.equal(404);
+            expect(response.body).to.have.key('error');
+            expect(response.body.error).to.equal('No valid id provided');
+        });
+        it('responds with code 401 and error message because of invalid token', async () => {
+            const response = await request(app)
+              .get('/challengesBody')
+              .set('Authorization', 'Bearer ölkxjdkljdglkjdgöljeöotuiöjkvlnvösodhg');
+            expect(response.body).to.be.a('object');
+            expect(response.statusCode).to.equal(401);
+            expect(response.body).to.have.key('error');
+            expect(response.body.error).to.equal('Invalid token');
+          });
+          it('responds with code 200 and array of challenges', async () => {
+            const response = await request(app)
+              .get('/challengesBody/1')
+              .set('Authorization', `Bearer ${token}`);
+            expect(response.body).to.be.a('object');
+            expect(response.statusCode).to.equal(200);
+            expect(response.body).to.have.key('data');
+            expect(response.body.data).to.be.a('array');
+            expect(response.body.data.length).to.greaterThan(0);
+          });
+         
+    });
+}); 
+
+
+
+
+
 
      describe('POST /challengesBody', () => {
          it('responds with code 400 and error message because of missing description', async () => {
